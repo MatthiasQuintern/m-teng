b1ab667336f73128d228bbc53b7f9af66a119c42
Matthias@Dell <matthiasqui@protonmail.com>
2023-04-27 01:41:06 +0200

    Changed Autozero value

--- scripts/smua_reset.lua
+++ scripts/smua_reset.lua
@@ -2,7 +2,8 @@
 smua.reset()
 smua.measure.autorangev = smua.AUTORANGE_ON
 smua.measure.autorangei = smua.AUTORANGE_ON
-smua.measure.autozero = smua.AUTOZERO_ONCE
+smua.measure.autozero = smua.AUTOZERO_AUTO
+-- smua.measure.autozero = smua.AUTOZERO_ONCE
 -- set output to 0A DC
 smua.source.output = smua.OUTPUT_OFF
 smua.source.func = smua.OUTPUT_DCAMPS
